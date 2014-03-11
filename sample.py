cb21fb35fae3f61c3e45ea219f779b5b24681334
Yoshiki Sato <yoshiki@xight.org>
2014-03-11 19:28:01 +0900

    add unittest

--- sample.py
+++ sample.py
@@ -1,13 +1,12 @@
 # coding: utf-8
 import os
 from zaimapi import Zaim
-
+from pprint import pprint
 
 consumer_key = unicode(os.environ.get("ZAIM_CONSUMER_KEY", ""))
 consumer_secret = unicode(os.environ.get("ZAIM_CONSUMER_SECRET", ""))
 access_token_key = unicode(os.environ.get("ZAIM_ACCESS_TOKEN_KEY", ""))
 access_token_secret = unicode(os.environ.get("ZAIM_ACCESS_TOKEN_SECRET", ""))
-
 
 def main():
     assert consumer_key, 'Please set "ZAIM_CONSUMER_KEY".'
@@ -16,12 +15,14 @@ def main():
     assert access_token_secret, 'Please set "ZAIM_ACCESS_TOKEN_SECRET".'
 
     zaim = Zaim(consumer_key, consumer_secret, access_token_key, access_token_secret)
-    print zaim.get_genres()
+    pprint(zaim.get_genres())
     print zaim.get_categories()
     print zaim.get_user_info()
     print zaim.get_currencies()
     print zaim.get_currency_sign("JPY")
     print zaim.get_money_records()
+    print zaim.get_genre_by_name(u"食料品")
+    print zaim.get_category_by_name(u"食費")
 
 if __name__ == '__main__':
     main()
---
+++ zaim_test.py
@@ -0,0 +1,79 @@
+# coding: utf-8
+import os
+import sys
+import codecs 
+import unittest
+
+from zaimapi import Zaim
+from pprint import pprint
+
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+
+consumer_key = unicode(os.environ.get("ZAIM_CONSUMER_KEY", ""))
+consumer_secret = unicode(os.environ.get("ZAIM_CONSUMER_SECRET", ""))
+access_token_key = unicode(os.environ.get("ZAIM_ACCESS_TOKEN_KEY", ""))
+access_token_secret = unicode(os.environ.get("ZAIM_ACCESS_TOKEN_SECRET", ""))
+assert consumer_key, 'Please set "ZAIM_CONSUMER_KEY".'
+assert consumer_secret, 'Please set "ZAIM_CONSUMER_SECRET".'
+assert access_token_key, 'Please set "ZAIM_ACCESS_TOKEN_KEY".'
+assert access_token_secret, 'Please set "ZAIM_ACCESS_TOKEN_SECRET".'
+
+zaim = Zaim(consumer_key, consumer_secret, access_token_key, access_token_secret)
+
+class ZaimClassTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    '''
+    def test_get_categories(self):
+        categories = zaim.get_categories(self)
+        pprint(categories)
+
+    def test_get_genres(self):
+        genres = zaim.get_genres(self)
+        pprint(genres)
+    '''
+    def test_get_genres_by_name(self):
+        genre = zaim.get_genre_by_name(u"食料品")
+        genre_org = {
+            u'active': 1,
+            u'category_id': 101,
+            u'created': u'2000-01-01 00:00:00',
+            u'id': 10101,
+            u'modified': u'2000-01-01 00:00:00',
+            u'name': u'\u98df\u6599\u54c1',
+            u'parent_genre_id': 10101,
+            u'sort': 1
+        }
+        self.assertEqual(genre,genre_org)
+
+    def test_get_genres_by_name_not_exist(self):
+        self.assertIsNone(zaim.get_genre_by_name(u"not exist"))
+
+    def test_get_category_by_name(self):
+        category = zaim.get_category_by_name(u"食費")
+        category_org = {
+            u'active': 1,
+            u'budget': 30000,
+            u'calc': u'variable',
+            u'color': u'#3a9625',
+            u'created': u'2000-01-01 00:00:00',
+            u'id': 101,
+            u'image': u'restaurant',
+            u'mode': u'payment',
+            u'modified': u'2000-01-01 00:00:00',
+            u'name': u'\u98df\u8cbb',
+            u'parent_category_id': 101,
+            u'sort': 1
+        }
+        self.assertEqual(category,category_org)
+
+    def test_get_category_by_name_not_exist(self):
+        self.assertIsNone(zaim.get_category_by_name(u"not exist"))
+
+if __name__ == '__main__':
+    unittest.main()
