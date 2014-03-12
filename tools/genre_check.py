f5f2b4faba334a0b5781c659e62c9900c0dccbf9
Yoshiki Sato <yoshiki@xight.org>
2014-03-12 18:42:38 +0900

    init

---
+++ tools/genre_check.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import codecs
+from datetime import datetime
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from zaimapi import Zaim
+from pprint import pprint
+
+sys.stdin  = codecs.getreader('utf8')(sys.stdin)
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+
+consumer_key = unicode(os.environ.get("ZAIM_CONSUMER_KEY", ""))
+consumer_secret = unicode(os.environ.get("ZAIM_CONSUMER_SECRET", ""))
+access_token_key = unicode(os.environ.get("ZAIM_ACCESS_TOKEN_KEY", ""))
+access_token_secret = unicode(os.environ.get("ZAIM_ACCESS_TOKEN_SECRET", ""))
+
+def main():
+    assert consumer_key, 'Please set "ZAIM_CONSUMER_KEY".'
+    assert consumer_secret, 'Please set "ZAIM_CONSUMER_SECRET".'
+    assert access_token_key, 'Please set "ZAIM_ACCESS_TOKEN_KEY".'
+    assert access_token_secret, 'Please set "ZAIM_ACCESS_TOKEN_SECRET".'
+
+    zaim = Zaim(consumer_key, consumer_secret, access_token_key, access_token_secret)
+
+    for line in sys.stdin:
+        itemList = line[:-1].split('\t')
+        (date, place, genre_name, comment, amount, account_name) = tuple(itemList)
+        try:
+            genre = zaim.get_genre_by_name(genre_name)
+        except ValueError:
+            print genre_name + " not found."
+
+if __name__ == '__main__':
+    main()
